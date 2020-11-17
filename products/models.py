4550129c29df821a70a6b5222850ec14628939cf
MargitStar <sovenokbymargo@mail.ru>
2020-11-17 18:00:38 +0300

    Add MainDishes bd

--- products/models.py
+++ products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class MainDishes(models.Model):
+    name = models.CharField(
+        'Name',
+        max_length=100
+    )
+
+    cover_photo = models.ImageField(
+        'Photo'
+    )
+
+    price = models.DecimalField(
+        'Price',
+        max_digits=5,
+        decimal_places=5
+    )
+
+    created_date = models.DateField(
+        'Created Date',
+        auto_now_add=True
+    )
+
+    updated_date = models.DateField(
+        'Updated Date',
+        auto_now=True
+    )
+
+    quantity = models.PositiveIntegerField(
+        'Amount',
+    )
+
+    weight = models.PositiveIntegerField(
+        'Weight',
+    )
+
+    ingredients = models.CharField(
+        'Ingredients',
+        max_length=200
+    )
