f52c874443538ae3e3cd8358f864469e34a4334f
MargitStar <sovenokbymargo@mail.ru>
2020-11-17 19:32:37 +0300

    Add view of JSON for dishes list

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from . import models
+from .serializer import MainDishesSerializer
+
+# TODO: return image url!
+class DishesList(APIView):
+
+    def get(self, request, format=None):
+        dishes = models.MainDish.objects.all()
+        serializer = MainDishesSerializer(dishes, many=True)
+        return Response(serializer.data)
