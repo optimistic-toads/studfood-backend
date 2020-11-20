e47fc57be7a4c5f9989e06ea34360a9f89863a6b
MargitStar <sovenokbymargo@mail.ru>
2020-11-20 16:22:20 +0300

    Changed serializer name

--- products/serializer.py
+++ products/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 
-class ProductsSerializer(serializers.Serializer):
+class ProductsListSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField(
 
     )
