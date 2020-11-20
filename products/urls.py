792d18fc168d2bb486b360d99803d0f720018847
MargitStar <sovenokbymargo@mail.ru>
2020-11-20 16:22:54 +0300

    Add url for detail view

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'dishes'
 
 urlpatterns = [
-    path('', views.ProductsList.as_view(), name='dishes_list')
+    path('', views.ProductsList.as_view(), name='dishes_list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_view')
 ]
