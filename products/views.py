c398ad08fc518573566c9364fe71fe928bfcb5ca
MargitStar <sovenokbymargo@mail.ru>
2020-11-20 16:24:05 +0300

    Add product detail view

--- products/views.py
+++ products/views.py
@@ -4,12 +4,26 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from . import models
-from .serializer import ProductsSerializer
+from .serializer import ProductsListSerializer
 
 
 class ProductsList(APIView):
 
     def get(self, request, format=None):
         dishes = models.Product.objects.all()
-        serializer = ProductsSerializer(dishes, context={'request': request}, many=True)
+        serializer = ProductsListSerializer(dishes, context={'request': request}, many=True)
+        return Response(serializer.data)
+
+
+class ProductDetailView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return models.Product.objects.get(pk=pk)
+        except models.Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductsListSerializer(product, context={'request': request})
         return Response(serializer.data)
