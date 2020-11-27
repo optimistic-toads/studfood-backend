0ac1c0ad3301865bc2968bc31d4f09ee88ac7957
MargitStar <sovenokbymargo@mail.ru>
2020-11-27 15:12:00 +0300

    Add get and post requests

--- order/views.py
+++ order/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Order
+from products.models import Product
+from .serializer import OrderSerializer
 
-# Create your views here.
+
+class OrderView(APIView):
+
+    def get(self, request):
+        snippets = Order.objects.all()
+        serializer = OrderSerializer(snippets, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request):
+        product = request.POST.get('product')
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            order_saved = serializer.save()
+        return Response({"success": "Article '{}' created successfully".format(order_saved)})
