cbcd5ecea0173bea66d3075831533a3fc669b2c7
MargitStar <sovenokbymargo@mail.ru>
2020-11-27 15:11:00 +0300

    Add order model

--- order/models.py
+++ order/models.py
@@ -1,2 +1,23 @@
 from django.db import models
+from products import models as product_model
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user'
+    )
+
+    product = models.ManyToManyField(
+        product_model.Product
+    )
+
+    created_date = models.DateTimeField(
+        'Time',
+        blank=True,
+        null=True
+    )
