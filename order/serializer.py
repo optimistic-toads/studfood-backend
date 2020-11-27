2e58c34fae002933ced0b99d384bbb770511bc98
MargitStar <sovenokbymargo@mail.ru>
2020-11-27 15:11:12 +0300

    Add order serializer

---
+++ order/serializer.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Order, User
+from products.models import Product
+from products.serializer import ProductsListSerializer
+
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+
+
+class OrderSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+
+    product = ProductSerializer(many=True)
+
+    created_date = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        product = validated_data.pop('product', [])
+        order = Order.objects.create(**validated_data)
+
+        for product_data in product:
+            prod = Product.objects.get(pk=product_data.get('id'))
+            order.product.add(prod)
+        return order
