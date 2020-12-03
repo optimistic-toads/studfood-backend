aeeb37a5a6dd4fed6647418c726196a360a238a6
MargitStar <sovenokbymargo@mail.ru>
2020-12-03 23:08:52 +0300

    Add urls for registration

--- StudFood/urls.py
+++ StudFood/urls.py
@@ -19,12 +19,17 @@ from django.urls import path, include
 from .settings import DEBUG
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt import views as jwt_views
+from authentication import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dishes/', include('products.urls', namespace='dishes')),
     path('orders/', include('order.urls', namespace='order')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('user/', include('authentication.urls', namespace='user'))
+
 ]
 
 if DEBUG:
